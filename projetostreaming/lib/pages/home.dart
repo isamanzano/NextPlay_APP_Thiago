0dc3ae09430292980b3de8ded7c77f427ffb0c95
isamanzano <isabelacmanzano05@gmail.com>
2024-10-16 17:24:50 -0300

    oi

--- projetostreaming/lib/pages/home.dart
+++ projetostreaming/lib/pages/home.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:projetostreaming/pages/cadastro.dart';
 import 'package:projetostreaming/shared/style.dart';
 
+class HomeTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext){
+    return MaterialApp(
+      title: '',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: Home(),
+    );
+  }
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -9,28 +22,46 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-
 class _HomeState extends State<Home> {
   final _formKey = GlobalKey<FormState>();
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-     
-   
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: const Color.fromARGB(255, 163, 213, 247),
+        selectedIndex: currentPageIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home'
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: 'Pesquisa',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history),
+            label: 'Histórico',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Você',
+          ),
+        ],
+      ),
+
       body: Container(
         height: MediaQuery.of(context).size.height * 1,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              MyColors.gradient1, MyColors.gradient2
-            ],
-            stops: [0.0, 0.8], // Posições das cores no degradê
-            begin: Alignment.topCenter,
-            end: Alignment.bottomRight,
-          ),
-        ),
+        
+
         ),
       );
   }
