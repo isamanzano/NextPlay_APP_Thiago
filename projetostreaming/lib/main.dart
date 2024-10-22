6cb65debcbb123fd0786b4c7fe302a3bc68ae80f
isamanzano <isabelacmanzano@gmail.com>
2024-10-22 16:40:34 -0300

    oi

--- projetostreaming/lib/main.dart
+++ projetostreaming/lib/main.dart
@@ -14,12 +14,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system, //definidr tema claro ou escuro
+      themeMode: ThemeMode.system, 
       theme: ThemeData(
         fontFamily: MyFonts.fontPrimary,
         colorScheme: ColorScheme.fromSeed(seedColor: MyColors.azulEscuro),
@@ -30,7 +29,7 @@ class MyApp extends StatelessWidget {
       '/splash' : (context) => SplashScreen(),
       '/login' : (context) =>  Login(),
       '/cadastro' : (context) => const Cadastro(),
-      '/home' : (context) => const Home(),
+      '/home' : (context) => Home(),
      },
     ); 
   }
--- projetostreaming/lib/pages/home.dart
+++ projetostreaming/lib/pages/home.dart
@@ -2,57 +2,109 @@ import 'package:flutter/material.dart';
 import 'package:projetostreaming/pages/cadastro.dart';
 import 'package:projetostreaming/shared/style.dart';
 
-class HomeTheme extends StatelessWidget {
+void main(){
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: Home(),
+      home: HomeScreen(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
 
+class HomeScreen extends StatefulWidget {
   @override
-  State<Home> createState() => _HomeState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeState extends State<Home> {
-  final _formKey = GlobalKey<FormState>();
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String? _selectedValue; 
+  bool _isDropdownOpened = false; 
+
+  void _toggleDropdown() {
+    setState(() {
+      _isDropdownOpened = !_isDropdownOpened;
+    });
+  }
   int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
+    return Scaffold(key: _scaffoldKey,
+      appBar: AppBar(
+        elevation: 0, 
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        )
+      ),
+
+      //DROPDOWN
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Conta'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.video_library),
+              title: Text('Categorias'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              }, 
+            ), 
+          ],
+        ),
+      ),
+
+      //NAVBAR
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
             currentPageIndex = index;
           });
         },
-        indicatorColor: const Color.fromARGB(255, 163, 213, 247),
+        indicatorColor: Colors.blue,
         selectedIndex: currentPageIndex,
         destinations: [
           NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home'
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Home', 
           ),
           NavigationDestination(
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.search, color: Colors.white),
             label: 'Pesquisa',
           ),
           NavigationDestination(
-            icon: Icon(Icons.history),
+            icon: Icon(Icons.history, color: Colors.white),
             label: 'Histórico',
           ),
           NavigationDestination(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person, color: Colors.white),
             label: 'Você',
           ),
         ],
